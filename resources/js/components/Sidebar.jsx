fa256ce172f98c85bcdaca489b62bb1f278f752c
Xiamir Luquez <alexanderx105@hotmail.com>
2026-06-04 17:25:02 -0500

    feat: hide Parameters nav item for Supervisor

--- resources/js/components/Sidebar.jsx
+++ resources/js/components/Sidebar.jsx
@@ -1,33 +1,39 @@
 import React from 'react';
 import { FaHome, FaChartBar, FaCog } from 'react-icons/fa';
 import { Link, useLocation } from 'react-router-dom';
+import { useUser } from './UserContext';
 
 const Sidebar = () => {
     const location = useLocation();
+    const { user } = useUser();
+    const esSupervisor = user?.tipo_usuario === "Supervisor";
+
     return (
         <nav className="sidebar">
-            <Link 
+            <Link
                 to="/dashboard"
                 className={`sidebar-icon ${location.pathname === '/dashboard' ? 'active' : ''}`}
             >
                 <FaHome size={24} />
             </Link>
 
-         
-            <Link 
+            <Link
                 to="/reports"
                 className={`sidebar-icon ${location.pathname === '/reports' ? 'active' : ''}`}
             >
                 <FaChartBar size={24} />
             </Link>
-            <Link 
-                to="/parameters"
-                className={`sidebar-icon ${location.pathname === '/parameters' ? 'active' : ''}`}
-            >
-                <FaCog size={24} />
-            </Link>
+
+            {!esSupervisor && (
+                <Link
+                    to="/parameters"
+                    className={`sidebar-icon ${location.pathname === '/parameters' ? 'active' : ''}`}
+                >
+                    <FaCog size={24} />
+                </Link>
+            )}
         </nav>
     );
 };
 
-export default Sidebar; 
+export default Sidebar;
